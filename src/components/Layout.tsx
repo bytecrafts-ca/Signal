82decac03743345a19b1d57580de4ca957a26f42
bytecrafts-ca <bytecrafts.ca@gmail.com>
2026-03-05 16:28:15 -0500

    awd

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,18 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import { Header } from './Header'
 import { Footer } from './Footer'
 
 export function Layout() {
+  const { pathname } = useLocation()
+
+  // Notify ad script on every route change so ads can refresh for the new page
+  useEffect(() => {
+    window.dispatchEvent(new CustomEvent('pageview', { detail: { path: pathname } }))
+    const w = window as Window & { apitiny?: { refresh?: () => void } }
+    if (typeof w.apitiny?.refresh === 'function') w.apitiny.refresh()
+  }, [pathname])
+
   return (
     <>
       <a href="#main" className="skip-link">Skip to content</a>
